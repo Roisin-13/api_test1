70c4fc297e80df8f6005ea387f2572fa90cec5fa
Roisin-13 <Roisin-13@users.noreply.github.com>
2023-08-14 21:01:10 +0100

    files added for api test - basic http

---
+++ script.ts
@@ -0,0 +1,19 @@
+let button = document.getElementById("advbtn"); 
+
+const clickHandler = () => {
+    let bar = fetch("https://api.adviceslip.com/advice")
+        .then((response) => response.json())
+        .then((foo) => {
+            console.log(foo.slip.advice)
+            console.log(foo.slip.id)
+            let header = document.getElementById("hi");
+            header!.innerHTML = foo.slip.advice;
+        })
+        .then((foo) => { console.log(typeof foo, "poop") })
+}
+
+
+
+button!.addEventListener("click", clickHandler);
+
+    
