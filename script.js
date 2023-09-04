5d14b1ae5f15a8f18ee30dd5434b2f86bedd5296
Roisin-13 <Roisin-13@users.noreply.github.com>
2023-09-04 15:35:55 +0100

    trying to type - oh dear

--- script.js
+++ script.js
@@ -2,15 +2,21 @@ var button = document.getElementById("advbtn");
 var clickHandler = function () {
     fetch("https://api.adviceslip.com/advice")
         .then(function (response) { return response.json(); })
-        .then(function (foo) {
-        console.log(foo.slip.advice);
-        console.log(foo.slip.id);
+        .then(function (res) {
+        console.log(res.advice);
+        console.log(res.id);
+        console.log(res);
+        console.log(res.slip);
+        console.log(typeof res, "hi");
+        console.log(typeof res.slip, "hello");
+        // let header = document.getElementById("hi");
+        // header!.innerHTML = res.slip.advice;
+        // let idNum = document.getElementById("number");
+        // idNum!.innerHTML = res.slip.id;
+    })["catch"](function (err) {
+        console.log(typeof err, "poop");
         var header = document.getElementById("hi");
-        header.innerHTML = foo.slip.advice;
-    })["catch"](function (foo) {
-        console.log(typeof foo, "poop");
-        var header = document.getElementById("hi");
-        header.innerHTML = "This isn't advice - please help I'm broken 😭";
+        header.innerHTML = "This isn't advice - I'm broken 😭";
     });
 };
 button.addEventListener("click", clickHandler);
--- script.ts
+++ script.ts
@@ -1,18 +1,29 @@
 let button = document.getElementById("advbtn"); 
 
 const clickHandler = () => {
+    interface Advice {
+        slip: object;
+        id: number; 
+        advice: string
+    }
     fetch("https://api.adviceslip.com/advice")
         .then((response) => response.json())
-        .then((foo) => {
-            console.log(foo.slip.advice)
-            console.log(foo.slip.id)
-            let header = document.getElementById("hi");
-            header!.innerHTML = foo.slip.advice;
+        .then((res : Advice) => {
+            console.log(res.advice)
+            console.log(res.id)
+            console.log(res)
+            console.log(res.slip)
+            console.log(typeof res, "hi")
+            console.log(typeof res.slip, "hello")
+            // let header = document.getElementById("hi");
+            // header!.innerHTML = res.slip.advice;
+            // let idNum = document.getElementById("number");
+            // idNum!.innerHTML = res.slip.id;
         })
-        .catch((foo) => { 
-            console.log(typeof foo, "poop")
+        .catch((err) => { 
+            console.log(typeof err, "poop")
             let header = document.getElementById("hi");
-            header!.innerHTML = "This isn't advice - please help I'm broken 😭";
+            header!.innerHTML = "This isn't advice - I'm broken 😭";
          })
 }
 
